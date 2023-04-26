a0d8008e49cce940a5f78f3fcc52f1b4aeddfc06
William Stadtlander <wastadtlander@vt.edu>
2023-04-25 22:43:11 -0400

    basic components

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from PyQt6.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QPlainTextEdit,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class WindowWidget(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.initUI()
 
-if __name__ == "__main__":
+    def initUI(self):
+        hLayout = QHBoxLayout()
+        infoDisplay = QPlainTextEdit()
+        vLayout = QVBoxLayout()
+        wakeTime = QTimeEdit()
+        sleepTime = QTimeEdit()
+
+        vLayout.addWidget(wakeTime)
+        vLayout.addWidget(sleepTime)
+        hLayout.addWidget(infoDisplay)
+        hLayout.addLayout(vLayout)
+        self.setLayout(hLayout)
+
+        self.setGeometry(400, 200, 400, 200)
+        self.setWindowTitle("waterlogged")
+        self.show()
+
+
+def main():
     app = QApplication([])
 
     window = WindowWidget()
-    window.show()
 
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
